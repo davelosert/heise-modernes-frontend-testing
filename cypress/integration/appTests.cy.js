15376caa8e08ea0fb8686e12e93f6128d14fc5d4
David Losert <david.losert@gmx.de>
2021-05-25 14:02:13 +0200

    Fixes cypress test after adding backend

--- cypress/integration/appTests.cy.js
+++ cypress/integration/appTests.cy.js
@@ -8,10 +8,19 @@ describe('Application Tests', () => {
   });
 
   it('allows to fill the form and shows a success message after submit.', () => {
+    cy.intercept({
+      method: 'GET',
+      url: 'http://localhost:3000/availableDates'
+    },
+    [{
+      date: '22.05.',
+      hours: [ '09:10 Uhr' ]
+    }]);
+
     cy.visit('http://localhost:8080');
 
     cy.contains('22.05.').click();
-    cy.contains('09:10').click();
+    cy.contains('09:10 Uhr').click();
     cy.findByText('Ihr Name:').type('Kim Müller');
     cy.findByText('Termin verbindlich buchen').click();
 
